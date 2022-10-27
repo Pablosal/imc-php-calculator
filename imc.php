fa0744fd744af05ed98e87aa152294947675653f
Pablo <pablosalgadou@gmail.com>
2022-10-27 12:09:11 -0300

    add calculation layouts

--- imc.php
+++ imc.php
@@ -1,4 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+  <title>Document</title>
+</head>
+
+<body>
+
+  <?php
   class IMCDetermination
   {
 
@@ -10,15 +24,15 @@
     {
 
       if ($imc < 18.5) {
-        return 'Bajo Peso';
+        return ['Bajo Peso', 'danger'];
       } else if ($imc >= 18.5 && $imc < 24.9) {
-        return 'Sobrepeso';
+        return ['Adecuado', 'success'];
       } else if ($imc >= 25 && $imc < 29.9) {
-        return 'Sobrepeso';
+        return ['Sobrepeso', 'primary'];
       } else if ($imc >= 30 && $imc < 39.9) {
-        return 'Obesidad';
+        return ['Obesidad', 'warning'];
       } else if ($imc > 40) {
-        return 'Obesidad Severa';
+        return ['Obesidad Severa', 'danger'];
       }
     }
   }
@@ -29,14 +43,28 @@
 
 
   $peso =  isset($_POST["peso"]) ?  $_POST["peso"] : 0;
-  $altura = isset($_POST["altura"]) ?  ($_POST["altura"]/ 100) *($_POST["altura"]/ 100)  : 0;
+  $altura = isset($_POST["altura"]) ?  ($_POST["altura"] / 100) * ($_POST["altura"] / 100)  : 0;
   $status;
-  
+
   $imc = $IMCDeterminator->obtenerIMC($peso, $altura);
-  $status = $IMCDeterminator->checkIMC($imc);
-  echo 'IMC';
-  echo $imc;
-  echo 'Status';
-  echo $status;
+  [$status, $alert] = $IMCDeterminator->checkIMC($imc);
+
 
   ?>
+
+  <div class="card">
+    <div class="card container">
+      <button type="button" class="btn btn-primary" >
+        IMC <span class="badge text-bg-success "> <?php echo $imc; ?> </span>
+      </button>
+
+      <div class="alert <?php echo "class= alert alert-". $alert; ?>" role="alert">
+        <?php echo $status; ?>
+      </div>
+    </div>
+
+
+  </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,30 +5,32 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <title>Document</title>
 </head>
 
 <body>
   <main>
-    <h1>Calcule su IMC</h1>
-    <form action="imc.php" method="POST">
-      <div class="form_group">
-        <label for="altura">Introduzca su altura</label>
-        <input type="text" name="altura" />
-        <small>En centimetros</small>
+    <form action="imc.php" class="card container" style="width: 18rem;" method="POST">
+      <div class="container card-body">
+        <h2 class="card-title">Calcule su IMC</h2>
+        <div class="col">
+          <label for="altura" class="form-label">Introduzca su altura</label>
+          <input type="text" name="altura" class="form-control" required />
+          <small class="text-muted">En centimetros</small>
+        </div>
+        <div class=" col ">
+          <label for="peso" class="form-label">Introduzca su peso</label>
+          <input type="text" name="peso" class="form-control" required />
+        </div>
       </div>
-      <div class="form_group">
-        <label for="peso">Introduzca su peso</label>
-        <input type="text" name="peso" />
-      </div>
-      <input type="submit" value="Calcular IMC">
+      <input type="submit" value="Calcular IMC" class="btn btn-success">
+      <br>
     </form>
-
   </main>
-
- 
 
 
 </body>
+
 
 </html>
