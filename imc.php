e6fb7c08fc2b429a4366ad22670c1ff839321e65
Pablo <pablosalgadou@gmail.com>
2022-11-06 14:57:13 -0300

    add comments

--- imc.php
+++ imc.php
@@ -10,19 +10,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-  <title>Document</title>
+  <title>Tu IMC</title>
 </head>
 
 <body>
 
   <?php
+  // Clase para obtener el imc y verificar los valores
   class IMCDetermination
   {
-
-    function obtenerIMC($peso, $altura)
+    //Esta funcion obtiene el imc
+    function getIMC($peso, $altura)
     {
       return round($peso / $altura,2);
     }
+    // Esta funcion verifica el rango de imc y entrega los valores finales y para los estilos de css
     function checkIMC($imc)
     {
 
@@ -41,15 +43,18 @@
     }
   }
 
+// Creamos una instancia de nuestra clase para usar sus funciones
+$IMCDeterminator = new IMCDetermination;
 
-  $IMCDeterminator = new IMCDetermination;
 
 
-
+// Verificamos que las variables se hayan enviado correctamente y luego le damos valores a las variables
   $peso =  isset($_POST["peso"]) ?  $_POST["peso"] : 0;
   $altura = isset($_POST["altura"]) ?  ($_POST["altura"] / 100) * ($_POST["altura"] / 100)  : 0;
   $status;
-  $imc = $IMCDeterminator->obtenerIMC($peso, $altura);
+  // Obtenemos los valores y los delegamos a sus respectivas variables
+  $imc = $IMCDeterminator->getIMC($peso, $altura);
+  // deestructuramos las variables de nuestro array para mayor facilidad
   [$status, $alert] = $IMCDeterminator->checkIMC($imc);
 
 
@@ -60,12 +65,15 @@
       <h2 class="card-title text-center">Su IMC es de</h2>
       <div class="col">
         <h1 class="text-center">
+          <!-- Entregamos el imc -->
           <span class="badge text-bg-success "> <?php echo $imc; ?> </span>
         </h1>
       </div>
       <div class=" col ">
         <h4 class="card-title text-center">Su condicion es de </h4>
+        <!-- Entregamos la clase para el status del imc -->
         <div class="alert <?php echo "class= text-center alert alert-" . $alert; ?>" role="alert">
+          <!-- Entregamos el estatus del imc -->
           <?php echo $status; ?>
         </div>
 
